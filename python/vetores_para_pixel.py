22363abede930f851751fdad4caa30d622451adc
exodojaffar <exodojaffar@gmail.com>
2021-01-30 16:11:36 -0400

    start turning into class and create a function for recreate a functin base on two points

--- python/vetores_para_pixel.py
+++ python/vetores_para_pixel.py
@@ -1,5 +1,48 @@
 
+class Screen():
+	"""docstring for Screen"""
+	def __init__(self, HEIGHT, WIDTH, STATE_ZERO = " "):
+		
+		self.__HEIGHT = HEIGHT
+		self.__WIDTH = WIDTH
+
+		self.__STATE_ZERO = STATE_ZERO
+		self.__SCREEN = list()
+
+		self.reset_screen()
+
+		pass
+		
+	def reset_screen(self) -> None:
+		for y in range(self.__HEIGHT):	
+			line = [self.__STATE_ZERO for x in range(self.__WIDTH)]
+
+			self.__SCREEN.append(line)
+			pass
+
+		pass
+
+	def draw_line(self, start_point: list, end_point: list) -> None:
+		X_1, Y_1 = start_point
+		X_2, Y_2 = end_point
+
+		def f(step: float) -> tuple:
+			a = (Y_2 - Y_1)/(X_2 - X_1)
+			b = Y_1 - a*X_1
+
+			y = a*step + b
+			return (step, y)
+
+		pass
+
 def main():
+	POINTS = list()
+
+	POINTS.append([[0,0], [5, 5]])
+
+	S = Screen(20, 20)
+	S.draw_line([1,8], [2,11])
+
 	pass
 
 if __name__ == '__main__':
