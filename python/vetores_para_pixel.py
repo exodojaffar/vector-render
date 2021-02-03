ccb29b1b8605f001273f3e699d890791d5667630
exodojaffar <exodojaffar@gmail.com>
2021-02-02 20:01:21 -0400

    Add functions for maneger screen, draw line, draw screen and reset

--- python/vetores_para_pixel.py
+++ python/vetores_para_pixel.py
@@ -22,27 +22,55 @@ class Screen():
 
 		pass
 
+	def draw_screen(self):
+		for linha in self.__SCREEN:
+			for pixel in linha:
+				print(pixel, end='')
+
+			print()
+
+		pass
+
 	def draw_line(self, start_point: list, end_point: list) -> None:
 		X_1, Y_1 = start_point
-		X_2, Y_2 = end_point
+		X_2, Y_2 = end_point	
 
-		def f(step: float) -> tuple:
+		def f(x: float) -> float:
 			a = (Y_2 - Y_1)/(X_2 - X_1)
 			b = Y_1 - a*X_1
 
-			y = a*step + b
-			return (step, y)
+			y = a*x + b
+
+			return y
+
+		step = 1 if X_1 <= X_2 else -1
+
+		for x in range(X_1,X_2+1, step):
+			y = round(f(x))
+
+			self.__SCREEN[y][x] = '#'
+
+			pass
 
 		pass
 
 def main():
-	POINTS = list()
+	# Triangulo
+	# POINTS = [[1,1], [5,5],[9,1]]
 
-	POINTS.append([[0,0], [5, 5]])
+	# Quadrado
+	POINTS = [[1,1], [15,1], [15,15], [1,15]]
 
-	S = Screen(20, 20)
-	S.draw_line([1,8], [2,11])
+	S = Screen(20, 20, '-')
 
+	for i in range(len(POINTS)-1):
+		s_point = POINTS[i]
+		for point in POINTS[i+1:]:
+			S.draw_line(s_point, point)
+
+		pass
+
+	S.draw_screen()
 	pass
 
 if __name__ == '__main__':
